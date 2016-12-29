a43138fc1d6e8fa217379f581819c21b4c4cd51e
Mark McFate <summitt.dweller@gmail.com>
2016-12-29 16:29:47 -0600

    Still experimenting.

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ con = None
 #   RKMaster with columns modelId, fileName and imagePath
 #   RKFace with columns modelId, personId, imageId
 
+print "Target is: " + vars.libraryDB
+
 try:
     con = lite.connect(vars.libraryDB)
     with con:
@@ -25,32 +27,40 @@ try:
         # data = cur.fetchone()
         # print "SQLite version: %s" % data
 
+        print "modelId, fingerprint, imagePath\n"
+
         # Now fetch some image info.
-        cur.execute("SELECT * FROM RKMaster") # " WHERE modelId < 101")
+        # Create an "images" dictionary of uuid:imagePath pairs.
+
+        images = {}
+
+        cur.execute("SELECT * FROM RKMaster WHERE modelId < 101")
         while True:
             row = cur.fetchone()
             if row == None:
                 break
-            print "%s %s %s" % (row["modelId"], row["fileName"], row["imagePath"])
+            print "%s %s %s" % (row["modelId"], row["fingerprint"], row["imagePath"])
+            images[row["uuid"]] = row["imagePath"]
             # print row[0], row[1]
 
         # Fetch and report a count of RKMaster records.
         cur.execute("SELECT count(modelId) FROM RKMaster")
         row = cur.fetchone()
-        print "There are " + str(row[0]) + " records in RKMaster."
+        print "\nThere are " + str(row[0]) + " records in RKMaster.\n"
 
         # rows = cur.fetchall()
         # for row in rows:
         #     print "%s %s %s" % (row["modelId"], row["fileName"], row["imagePath"])
 
 except lite.Error, e:
-    print "Error: %s:" % e.args[0]
+    print "Error: %s" % e.args[0]
     sys.exit(1)
     
 finally:
     
     if con:
         con.close()
+        sys.exit(1)
 
 
 # Connect to the Person.db.
@@ -58,29 +68,49 @@ finally:
 #   RKPerson with columns modelId, name
 #   RKFace with columns modelId, personId, imageId
 
+print "\n\nTarget is: " + vars.personDB
+
 try:
     con = lite.connect(vars.personDB)
     with con:
         con.row_factory = lite.Row  # use a dictionary cursor, the data is sent in the form of Python dictionaries. This way we can refer to the data by their column names.
         cur = con.cursor()
 
+        print "\n\nmodelId, uuid, name\n"
+
         # Now fetch some RKPerson info.
+        # Create a "people" dictionary of modelId:name pairs.
+
+        people = {}
+
         cur.execute("SELECT * FROM RKPerson")  # " WHERE modelId < 101")
         while True:
             row = cur.fetchone()
             if row == None:
                 break
-            print "%s %s" % (row["modelId"], row["name"])
+            print "%s %s %s" % (row["modelId"], row["uuid"], row["name"])
+            people[row["modelId"]] = row["name"]
             # print row[0], row[1]
 
+        print "\n\nmodelId, personId, imageId\n"
+
         # Now fetch some RKFace info.
-        cur.execute("SELECT * FROM RKFaces")  # " WHERE modelId < 101")
+
+        cur.execute("SELECT * FROM RKFace WHERE modelId < 101")
         while True:
             row = cur.fetchone()
             if row == None:
                 break
-            print "%s %s %s" % (row["modelId"], row["personId"], row["imageId"])
+            pid = row["personId"]
+            # print "%s %s %s" % (row["modelId"], pid, row["imageId"])
             # print row[0], row[1]
+
+            # If personId > 0 attempt to look up the person in people and report any that are found.
+            if pid > 0:
+                pName = people[pid]
+                image = row["uuid"]
+                path = images[image]
+                print pName + " is identified in the image at " + path
 
 
 except lite.Error, e:
--- vars.py
+++ vars.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-libraryDB = "/files/Fileserver Images Library.photoslibrary/database/Library.apdb"
-personDB = "/files/Fileserver Images Library.photoslibrary/database/Person.db"
-# libraryDB = "/Volumes/files/database/Library.apdb"
-# personDB = "/Volumes/files/database/Person.db"
+#libraryDB = "/Volumes/files/Fileserver Images Library.photoslibrary/database/Library.apdb"
+#personDB = "/Volumes/files/Fileserver Images Library.photoslibrary/database/Person.db"
+#libraryDB = "/Volumes/files/database/Library.apdb"
+#personDB = "/Volumes/files/database/Person.db"
+libraryDB = "./database/Library.apdb"
+personDB = "./database/Person.db"
