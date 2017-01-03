c3644a2400bd7a835862de72ca02a043ef4624c8
Mark McFate <summitt.dweller@gmail.com>
2017-01-03 17:47:20 -0600

    Still experimenting.

--- vars.py
+++ vars.py
@@ -5,5 +5,5 @@ libraryDB = "/Volumes/files/STORAGE/_PHOTOS/Photos.photoslibrary/database/photos
 #personDB = "/Volumes/files/Fileserver Images Library.photoslibrary/database/Person.db"
 #libraryDB = "/Volumes/files/database/Library.apdb"
 #personDB = "/Volumes/files/database/Person.db"
-libraryDB = "./database/photos.db"
+libraryDB = "/files/database/photos.db"
 #personDB = "./database/Person.db"
